3cc27cd9eb8ce7ea525313b2efab7e50a4e32357
Candygoblen123 <andrew.glaze76@gmail.com>
2023-02-02 14:50:29 -0500

    Initial Commit

---
+++ Trombone Champ Mod Manager/Trombone_Champ_Mod_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trombone_Champ_Mod_ManagerApp.swift
+//  Trombone Champ Mod Manager
+//
+//  Created by Andrew Glaze on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Trombone_Champ_Mod_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
